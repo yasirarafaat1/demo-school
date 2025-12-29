e8bde33145b9b2a86aee77aa110fd7a5137581aa
faizansiddqui <faizansiddiqui7317@gmail.com>
2025-12-29 13:24:50 +0530

    feat(analytics): add Vercel Speed Insights integration
    - Added @vercel/speed-insights dependency to package.json and package-lock.json
    - Imported SpeedInsights component from @vercel/speed-insights/react
    - Included <SpeedInsights /> component alongside <Analytics /> in ReactDOM render
    - Updated imports to use consistent double quotes and formatting

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<App />
-		<Analytics />
-	</React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+    <Analytics />
+    <SpeedInsights />
+  </React.StrictMode>
+);
